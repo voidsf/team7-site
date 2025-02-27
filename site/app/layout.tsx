1f9946e1cc012b790e74df55f71d461ccde4ab94
Samantha Fisher <fishers14@cardiff.ac.uk>
2025-02-27 18:32:05 +0000

    remove unused pages

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -47,17 +47,7 @@ export default function RootLayout({
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
+            
           </div>
         </Providers>
       </body>
