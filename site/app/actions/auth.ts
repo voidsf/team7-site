52f403471624c565ba245efe2d1de9068d380165
Samantha Fisher <fishers14@cardiff.ac.uk>
2025-03-06 13:40:19 +0000

    create user if does not already exist, returns error code

--- site/app/actions/auth.ts
+++ site/app/actions/auth.ts
@@ -1,20 +1,40 @@
-"use server"
+"use server";
 
-import { SignupFormValidation } from "../lib/definitions"
-import { FormState } from "../lib/definitions"
+import { createUser, DatabaseRequestStatus } from "database";
+
+import { SignupFormValidation } from "../lib/definitions";
+import { FormState } from "../lib/definitions";
+
 
 export async function signup(state: FormState, formData: FormData) {
-    const validFields = SignupFormValidation.safeParse({
-        name: formData.get("name"),
-        email: formData.get("email"),
-        password: formData.get("password"),
-    })
+  const validFields = SignupFormValidation.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
 
-    if (!validFields.success){
-        return {
-            errors: validFields.error.flatten().fieldErrors
-        }
-    }
+  if (!validFields.success) {
+    return {
+      errors: validFields.error.flatten().fieldErrors,
+    };
+  }
 
-    // create user
+  // create user
+  const status: DatabaseRequestStatus = await createUser("database.db", {
+    name: validFields.data.name,
+    email: validFields.data.email,
+    pass: validFields.data.password,
+  });
+
+  switch (status.code) {
+    case 0: 
+      return { success: true };
+    case 2:
+      return { errors: { email: "A user with this email already exists" } };
+    default: 
+      return { errors: { email: "An error occurred" } };
+  };
+
+  
+
 }
